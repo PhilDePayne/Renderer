3a67b574c389ef05a3855c4d4a92cbcd32ea8827
Markus <markusderiu610@gmail.com>
2023-03-06 15:40:54 +0100

    Add Ray, Sphere and Plane classes

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,10 @@
 #include "Rasterizer.h"
 #include "Vec3.h"
 
-int main()
-{
-    unsigned int width = 256;
-    unsigned int height = 256;
-    unsigned int color = 0xff7caf31;
+void FGK() {
 
-    Buffer* buffer = new Buffer(width, height, color);
-
-    Writer* writer = new Writer();
-
-    Rasterizer* rasterizer = new Rasterizer(*buffer);
-
-    Vec3<float>* x = new Vec3<float>(0.0f, 1.0f, 0.0f);
-    Vec3<float>* y = new Vec3<float>(1.0f, -1.0f, 0.0f);
-    Vec3<float>* z = new Vec3<float>(-1.0f, -1.0f, 0.0f);
-
-    Triangle* triangle = new Triangle(*x, *y, *z);
+    Vec3<float>* x = new Vec3<float>(0.0f, 3.0f, 0.0f);
+    Vec3<float>* y = new Vec3<float>(5.0f, 5.0f, 0.0f);
 
     *x += *y;
 
@@ -35,6 +22,43 @@ int main()
     *y -= *x;
 
     std::cout << x->angleBetween(*y) << '\n';
+
+    Vec3<float>* a = new Vec3<float>(4.0f, 5.0f, 1.0f);
+    Vec3<float>* b = new Vec3<float>(4.0f, 1.0f, 3.0f);
+    Vec3<float> c = a->cross(*b);
+
+    std::cout << c.x << " " << c.y << " " << c.z << '\n';
+
+    c.normalize();
+
+    std::cout << c.x << " " << c.y << " " << c.z << '\n';
+
+    delete x;
+    delete y;
+    delete a;
+    delete b;
+
+}
+
+int main()
+{
+    unsigned int width = 256;
+    unsigned int height = 256;
+    unsigned int color = 0xff7caf31;
+
+    FGK();
+
+    Buffer* buffer = new Buffer(width, height, color);
+
+    Writer* writer = new Writer();
+
+    Rasterizer* rasterizer = new Rasterizer(*buffer);
+
+    Vec3<float>* x = new Vec3<float>(0.0f, 1.0f, 0.0f);
+    Vec3<float>* y = new Vec3<float>(1.0f, -1.0f, 0.0f);
+    Vec3<float>* z = new Vec3<float>(-1.0f, -1.0f, 0.0f);
+
+    Triangle* triangle = new Triangle(*x, *y, *z);
 
     rasterizer->drawTriangle(*triangle, 0xff012345);
 
